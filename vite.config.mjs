405a660695f0cb545d369accae0a80d24a31bf95
Haoqun Jiang <haoqunjiang@gmail.com>
2021-03-26 21:29:16 +0800

    refactor: let's use .mjs to go full ESM

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,10 @@
 import path from 'path'
+import { URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
